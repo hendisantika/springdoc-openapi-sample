e6a186f39ebf59c26109b2af962b372be6a6d878
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-26 13:29:00 +0700

    #3 - Add Person Model

---
+++ src/main/java/com/hendisantika/springdocopenapisample/model/Person.java
@@ -0,0 +1,55 @@
+package com.hendisantika.springdocopenapisample.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springdoc-openapi-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 26/04/20
+ * Time: 13.27
+ */
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.CreditCardNumber;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "person")
+@XmlAccessorType(XmlAccessType.FIELD)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+    private long id;
+
+    private String firstName;
+
+    @NotNull
+    @NotBlank
+    private String lastName;
+
+    @Pattern(regexp = ".+@.+\\..+", message = "Please provide a valid email address")
+    private String email;
+
+    @Email()
+    private String email1;
+
+    @Min(18)
+    @Max(30)
+    private int age;
+
+    @CreditCardNumber
+    private String creditCardNumber;
+
+}
