8c368811dcfa8d414fba28dc406c7d5a41f1c96d
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-26 13:19:13 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springdocopenapisample/SpringdocOpenapiSampleApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springdocopenapisample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdocOpenapiSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringdocOpenapiSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springdocopenapisample/SpringdocOpenapiSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springdocopenapisample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdocOpenapiSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
