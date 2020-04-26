b871c3363f7622af7f099adf38a8f4b796c59de3
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-26 14:40:25 +0700

    #11 - Add QualifiedTypeNameResolver in CustomConverter

--- src/main/java/com/hendisantika/springdocopenapisample/CustomConverter.java
+++ src/main/java/com/hendisantika/springdocopenapisample/CustomConverter.java
@@ -3,6 +3,10 @@ package com.hendisantika.springdocopenapisample;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.v3.core.converter.ModelConverters;
 import io.swagger.v3.core.jackson.ModelResolver;
+import io.swagger.v3.core.jackson.TypeNameResolver;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Set;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,5 +25,21 @@ class CustomConverter extends ModelResolver {
     public static void add(ObjectMapper ojectMapper) {
         ModelConverters.getInstance().addConverter(new CustomConverter(ojectMapper));
     }
+
+    static class QualifiedTypeNameResolver extends TypeNameResolver {
+        @Override
+        protected String nameForClass(Class<?> cls, Set<Options> options) {
+            String className = cls.getName().startsWith("java.") ? cls.getSimpleName() : cls.getName();
+            if (options.contains(Options.SKIP_API_MODEL)) {
+                return className;
+            }
+            final io.swagger.v3.oas.annotations.media.Schema model =
+                    cls.getAnnotation(io.swagger.v3.oas.annotations.media.Schema.class);
+            final String modelName = model == null ? null : StringUtils.trimToNull(model.name());
+            return modelName == null ? className : modelName;
+        }
+
+    }
+
 
 }
